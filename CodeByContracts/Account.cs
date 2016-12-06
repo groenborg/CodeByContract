416dc3ff67cb76f953a698ef1fd0a7df8f542e94
Christopher Mortensen (CSMO) <csmo@oticonmedical.com>
2016-12-06 11:13:51 +0100

    Initial Commit with all the useful cs-files

---
+++ CodeByContracts/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace CodeByContracts {
+    class Account {
+
+        public string Name { get; set; }
+
+        public double Balance { get; set; }
+
+        public Account(string name, double balance) {
+            this.Name = name;
+            this.Balance = balance;
+        }
+        
+        [ContractInvariantMethod]
+        private void ObjectInvariant() {
+            Contract.Invariant(Balance >= 0);
+            Contract.Invariant(!String.IsNullOrWhiteSpace(Name));
+        }
+        
+        [ContractAbbreviator]
+        private void TransactionRules(double amount) {
+            Contract.Requires(amount > 0, "Amount should be positive.");
+            Contract.Requires<ArgumentException>(amount > 0, "Exception");
+        }
+
+        public double Withdraw(double amount) {
+            Contract.Ensures(Contract.Result<double>() < Contract.OldValue(Balance));
+            TransactionRules(amount);
+            
+            return Balance -= amount;
+        } 
+
+
+        public double Deposit(double amount) {
+            Contract.Ensures(Contract.Result<double>() > Contract.OldValue(Balance));
+            TransactionRules(amount);
+            
+            return Balance += amount;
+        }
+
+
+
+    }
+}
---
+++ CodeByContracts/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeByContracts {
+    class Program {
+        static void Main(string[] args) {
+            Account account = new Account("Roberts Konto", 18);
+
+            account.Withdraw(20);
+
+            Console.WriteLine("Account Balance: {0}", account.Balance);
+            Console.ReadKey();
+        }
+    }
+}
